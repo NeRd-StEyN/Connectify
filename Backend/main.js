d4a9b708cfbf1c95c5a1104b250d2e7a2c0e80f3
NeRd-StEyN <nipun0411@gmail.com>
2025-07-19 15:55:20 +0530

    Update CORS origin and .env for deployment

--- Backend/main.js
+++ Backend/main.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({origin: "https://connectify-frontend.vercel.app", credentials: true }));
+app.use(cors({origin: "https://connectify2025.vercel.app/", credentials: true }));
 
 
 
