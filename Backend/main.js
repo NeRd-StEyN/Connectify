6987cb9d6283bd0529e63ab6ef94a1d257bbac81
NeRd-StEyN <nipun0411@gmail.com>
2025-07-19 16:47:04 +0530

    Update main.js

--- Backend/main.js
+++ Backend/main.js
@@ -21,7 +21,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({origin: "https://connectify2025.vercel.app", credentials: true }));
+app.use(cors({
+  origin: ["http://localhost:5173", "https://connectify2025.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 
 
