97b4e840ece5004a229bb5d25f2a62171cb3c45f
NeRd-StEyN <nipun0411@gmail.com>
2025-07-20 16:39:17 +0530

    Create Spinner.jsx

---
+++ Frontend/src/import React from "react"; import "./Spinner.css";  const Spinner = () => {   return (     <div className="modern-spinner-overlay">       <div className="modern-spinner"></div>     </Spinner.jsx
@@ -0,0 +1,12 @@
+
+import "./Spinner.css";
+
+export const Spinner = () => {
+  return (
+    <div className="modern-spinner-overlay">
+      <div className="modern-spinner"></div>
+    </div>
+  );
+};
+
+
